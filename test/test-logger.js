e932a28c62fc450ed182e7c9ef31b3848d3c173b
Sebastian Herrlinger <sebastian@formzoo.com>
2012-02-22 05:03:20 -0800

    Test & fix STDOUT log

--- test/test-logger.js
+++ test/test-logger.js
@@ -3,3 +3,7 @@ var Logger = require('../lib/logger.js').Logger;
 // Test logging to file
 var log = new Logger(__dirname + '/test.log', 'debug');
 log.debug('Test output');
+
+// Test logging to console
+var log = new Logger(null, 'debug');
+log.debug('Test output');
